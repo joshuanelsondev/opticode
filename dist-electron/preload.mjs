77cf1dc0cc4b78cf4ad3561ee4e896f3a340cac1
joshuanelsondev <joshuanelsondev@gmail.com>
2025-02-03 00:21:10 -0500

    build: update preload script to set up IPC communication

--- dist-electron/preload.mjs
+++ dist-electron/preload.mjs
@@ -1,21 +1,21 @@
 "use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("ipcRenderer", {
+import { contextBridge, ipcRenderer } from "electron";
+contextBridge.exposeInMainWorld("ipcRenderer", {
   on(...args) {
     const [channel, listener] = args;
-    return electron.ipcRenderer.on(channel, (event, ...args2) => listener(event, ...args2));
+    return ipcRenderer.on(channel, (event, ...args2) => listener(event, ...args2));
   },
   off(...args) {
     const [channel, ...omit] = args;
-    return electron.ipcRenderer.off(channel, ...omit);
+    return ipcRenderer.off(channel, ...omit);
   },
   send(...args) {
     const [channel, ...omit] = args;
-    return electron.ipcRenderer.send(channel, ...omit);
+    return ipcRenderer.send(channel, ...omit);
   },
   invoke(...args) {
     const [channel, ...omit] = args;
-    return electron.ipcRenderer.invoke(channel, ...omit);
+    return ipcRenderer.invoke(channel, ...omit);
   }
   // You can expose other APTs you need here.
   // ...
