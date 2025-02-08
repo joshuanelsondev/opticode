32ac81a1860622572e0168ade1e5c23e056e7cb5
joshuanelsondev <joshuanelsondev@gmail.com>
2025-02-07 23:05:25 -0500

    build: install jquery and add script to index.html file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.scss";
 
 function App() {
+  new Def.Autocompleter.Search(
+    "icd10",
+    "https://clinicaltables.nlm.nih.gov/api/icd10cm/v3/search?sf=code,name",
+    { tableFormat: true, valueCols: [0], colHeaders: ["Code", "Name"] }
+  );
   return (
     <>
       <h1 className="welcome">Welcome to Opticode</h1>
+      <input type="text" id="icd10" placeholder="Enter code or name" />
     </>
   );
 }
