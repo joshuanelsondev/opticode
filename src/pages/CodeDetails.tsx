5ccd4eccc38139a9f4751ae665a9b708233b8eb6
joshuanelsondev <joshuanelsondev@gmail.com>
2025-02-23 15:36:02 -0500

    feat: create pages directory with different page components

---
+++ src/pages/CodeDetails.tsx
@@ -0,0 +1,7 @@
+export default function CodeDetails() {
+  return (
+    <div>
+      <h1>Code Details</h1>
+    </div>
+  );
+}
---
+++ src/pages/ScriptEditor.tsx
@@ -0,0 +1,7 @@
+export default function ScriptEditor() {
+  return (
+    <div>
+      <h1>Script Editor</h1>
+    </div>
+  );
+}
